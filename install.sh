f6e903979df6ce0380812f5eadfcccd2a533afde
André Freire <andrefreirecg@gmail.com>
2024-04-03 11:50:22 -0300

    first_commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+!#bin/bash
+cd ~
+sudo apt-get update -y
+
+
+# GIT
+sudo apt-get install git curl zsh -y
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install 20
+nvm use 20
+
+npm install -g yarn
+
+# Docker
+sudo apt-get install ca-certificates -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc 
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Devilbox
+git clone https://github.com/cytopia/devilbox
+cd devilbox
+cp env-example .env
+
+sudo apt-get install docker-compose -y
+
+sudo docker-compose up -d
